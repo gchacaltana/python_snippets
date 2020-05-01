f6b55249f0ca984bd02423941f2a3db55f5db508
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-04-30 23:29:47 -0500

    add first version sequences snippets

---
+++ sequences.py
@@ -0,0 +1,6 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+computerResearchers = ["Andrew NG", "Yann LeCun", "Yoshua Bengio", "Carol Reiley", "Sebastian Thrun", "Jeff Dean", "Pieter Abbeel"]
+print(computerResearchers[0:1])
+# Andrew NG
