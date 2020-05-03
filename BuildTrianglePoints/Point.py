4d36dedb8d172ddc0e69a932f1347673c7c7709e
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-02 23:52:13 -0500

    add Point in BuildTriangulePoints Module

---
+++ BuildTrianglePoints/Point.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__file__ = "Point.py"
+
+class Point(object):
+    def __init__(self,name, x,y):
+        self.name = name
+        self.x, self.y = x, y
