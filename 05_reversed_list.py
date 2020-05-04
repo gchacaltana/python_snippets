f5cbab276c2a0386bb3aabdd2fb559cae5870352
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-04 02:10:47 -0500

    HF code examples py

--- 05_reversed_list.py
+++ 05_reversed_list.py
@@ -4,9 +4,15 @@
 books_to_read = ["Python Data Science Handbook", "Understanding Machine Learning", "Linear Algebra Problem Book", "Naked Statistics", "Python for Data Analysis"]
 print(f"Libros ordenados por su indice")
 print([n for n in books_to_read])
+# Devuelve:
+# ['Python Data Science Handbook', 'Understanding Machine Learning', 'Linear Algebra Problem Book', 'Naked Statistics', 'Python for Data Analysis']
 
 print(f"\nLibros en orden inverso")
 print([n for n in reversed(books_to_read)])
+# Devuelve:
+# ['Python for Data Analysis', 'Naked Statistics', 'Linear Algebra Problem Book', 'Understanding Machine Learning', 'Python Data Science Handbook']
 
 print(f"\nLibros en ordenados por su nombre")
 print([n for n in sorted(books_to_read)])
+# Devuelve
+# ['Linear Algebra Problem Book', 'Naked Statistics', 'Python Data Science Handbook', 'Python for Data Analysis', 'Understanding Machine Learning']
