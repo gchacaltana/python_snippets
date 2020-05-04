b6cd11f198e9bc2f331047cfbf7529cabce11889
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-03 23:17:04 -0500

    code review list comprehension

--- 02_list_comprehensions.py
+++ 02_list_comprehensions.py
@@ -11,15 +11,15 @@ if __name__=='__main__':
     # Creamos la serie S, mediante comprensión de lista.
     s = [n for n in range(serie_start,serie_end+1,step)]
 
-    print(f"\nSerie:\nS = ",s)
+    print(f"\Sucesión:\nS = ",s)
     print(f"\nInicio: {serie_start}\tFin: {serie_end}\tSalto: {step}")
 
-    print(f"\nSerie S1 conformado por los numeros multiplos de {multiplo} de la serie S")
+    print(f"\Sucesión S1 conformado por los numeros multiplos de {multiplo} de la sucesión S")
     s1 = [n for n in range(serie_start,serie_end+1,step) if n%3==0]
     print("\nS1 = ", s1)
 
-    print(f"\nSerie S2 conformado por numeros elevados al cuadrado de la serie S1")
+    print(f"\Sucesión S2 conformado por numeros elevados al cuadrado de la sucesión S1")
     s2 = [pow(n,2) for n in s1]
     print("\nS2 = ",s2)
 
-    print(f"\nSuma de los numeros mayores a 2000 de la serie S2 = {sum(n for n in s2 if n>2000)}")
+    print(f"\nSuma de los numeros mayores a 2000 de la sucesión S2 = {sum(n for n in s2 if n>2000)}")
