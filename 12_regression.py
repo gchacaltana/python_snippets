e3bd4bd128a1e3c0fea2ca19596878ec28d04b53
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-07-06 23:07:13 -0500

    add data temperature and expansion alpha

--- 12_regression.py
+++ 12_regression.py
@@ -2,3 +2,12 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    # data temperatura
+    data_temperature = [80, 60, 40, 20, 0, -20, -40, -60, -80, -100, -120, -140, -
+                        160, -180, -200, -220, -240, -260, -280, -300, -320, -340]
+                        
+    # data coeficiente de expansion
+    data_thermal_expansion = [6.470, 6.360, 6.240, 6.120, 6.000, 5.860, 5.720, 5.580, 5.430, 5.280,
+                              5.090, 4.910, 4.720, 4.520, 4.300, 4.080, 3.830, 3.580, 3.330, 3.070, 2.760, 2.450]
